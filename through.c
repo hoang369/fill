41b0aa03ac2cee318b8b4038e1c2eb8c0a70f7f2
hoang369 <brigait.moattg@hotmail.com>
2025-02-25 16:08:12 +0800

    Modify through.c

--- through.c
+++ through.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Between various teacher where industry science.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
